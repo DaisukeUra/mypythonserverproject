fd9bef38f81a281edccee122280beb3424753f87
DaisukeURA <daisuke.ura.1230@gmail.com>
2017-04-01 21:41:17 +0900

    Mod: change cgi-server

--- simpleserver.py
+++ simpleserver.py
@@ -1,6 +1,6 @@
 import http.server
 
 server_address = ("", 8000)
-handler_class = http.server.SimpleHTTPRequestHandler #ハンドラを設定
-simple_server = http.server.HTTPServer(server_address, handler_class)
-simple_server.serve_forever()
+handler_class = http.server.CGIHTTPRequestHandler #1 ハンドラを設定
+server = http.server.HTTPServer(server_address, handler_class)
+server.serve_forever()
